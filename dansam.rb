6b95f41bdb04102ee1882e598b9300a782fec9c8
Daniel Samuel <dansam5000@gmail.com>
2021-04-20 12:15:08 +0100

    variables lesson

--- dansam.rb
+++ dansam.rb
@@ -1,12 +1,24 @@
+# prints to the screen
 print "Hello World"
 puts "Dansam"
 puts "Dansam"
 puts " is great"
 print "DanSam"
 print " Snr man"
+
+# drawing shapes
 puts""
 puts "    /|"
 puts "   / |"
 puts "  /  |"
 puts " /   |"
 puts "/____|"
+
+# variables
+char_name = "Isaac"
+char_age = "35"
+puts ("There once was a man named " + char_name)
+puts ("he was " + char_age + " years old.")
+char_name = "Jacob"
+puts ("He really liked the name " + char_name)
+puts ("but don't like being " + char_age)
