1f466e715bfef0f81c877d2fee4fee8a62fa3924
Daniel Samuel <dansam5000@gmail.com>
2021-04-20 12:25:35 +0100

    data types in ruby

--- dansam.rb
+++ dansam.rb
@@ -22,3 +22,11 @@ puts ("he was " + char_age + " years old.")
 char_name = "Jacob"
 puts ("He really liked the name " + char_name)
 puts ("but don't like being " + char_age)
+
+# data types in ruby
+name = "Daniel"  #string
+age = 75 #number
+gpa = 3.2 #float
+ismale = true #boolean
+istall = false #boolean
+flaws = nil # nil or empty value
