13aae7b2e2cc42b1c4df9f5bb666e5475c76737d
Daniel Samuel <dansam5000@gmail.com>
2021-04-21 17:27:32 +0100

    working with strings

--- dansam.rb
+++ dansam.rb
@@ -30,3 +30,14 @@ gpa = 3.2 #float
 ismale = true #boolean
 istall = false #boolean
 flaws = nil # nil or empty value
+
+# working with string methods
+phrase = "   Dan academy   "
+puts phrase.upcase() # convert string to uppercase
+puts phrase.downcase() # convert string to lowercase
+puts phrase.strip() # remove whitespace
+puts phrase.length() # figure out the nuber of char in string
+puts phrase.include? "academys" # to know if a prhase contain a word
+puts phrase[0] # get a char at index position
+puts phrase[0,4] #get a range of char 
+puts phrase.index("D") # get a char index
