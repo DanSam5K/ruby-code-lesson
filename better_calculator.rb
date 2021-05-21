9304a7af5feb7f32f8a4d7b9d8d3af90b9ee86e2
Daniel Samuel <dansam5000@gmail.com>
2021-05-22 00:55:42 +0100

    calcultor logic

--- better_calculator.rb
+++ better_calculator.rb
@@ -5,3 +5,14 @@ op = gets.chomp()
 puts "Enter second Number: "
 num2 = gets.chomp().to_f
 
+if op == "+"
+  puts (num1 + num2)
+elsif op == "-"
+  puts (num1 - num2)
+elsif op == "*"
+  puts (num1 * num2)
+elsif op == "/"
+  puts (num1 / num2)
+else
+  puts "Invalid Operator"
+end
