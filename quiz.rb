361d9369a1308cea3987fab1cfa646c321e20959
Daniel Samuel <dansam5000@gmail.com>
2021-06-02 18:28:19 +0100

    Add prompt questions

--- quiz.rb
+++ quiz.rb
@@ -5,3 +5,10 @@ class Question
         @answer = answer
     end
 end
+
+p1 = "What color are apples?\n(a)red\n(b)purple\n(c)orange"
+p2 = "What color are bananas?\n(a)pink\n(b)red\n(c)yellow"
+p3 = "What color are pears?\n(a)green\n(b)purple\n(c)orange"
+p4 = "How many time have a london team won champions league?\n(a)1\n(b)2\n(c)3"
+p5 = "What London team won champions league?\n(a)Chelsea\n(b)Arsenal\n(c)Tottenham"
+
