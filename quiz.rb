5ed642ce4f6be6e4ad3d5ec63868c83943498501
Daniel Samuel <dansam5000@gmail.com>
2021-06-02 18:33:48 +0100

    Add array of questions

--- quiz.rb
+++ quiz.rb
@@ -12,3 +12,10 @@ p3 = "What color are pears?\n(a)green\n(b)purple\n(c)orange"
 p4 = "How many time have a london team won champions league?\n(a)1\n(b)2\n(c)3"
 p5 = "What London team won champions league?\n(a)Chelsea\n(b)Arsenal\n(c)Tottenham"
 
+questions = [ 
+    Question.new(p1, "a"), 
+    Question.new(p2, "c"), 
+    Question.new(p3, "b"),
+    Question.new(p4, "b"),
+    Question.new(p5, "a")
+]
