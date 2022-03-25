97112f3285f133ef5981a527be52ce43f13b4781
Daniel Samuel <dansam5000@gmail.com>
2022-03-25 19:35:41 +0100

    Final changes project implementation

--- dice_game/roller.rb
+++ dice_game/roller.rb
@@ -1,5 +1,6 @@
 
-class Die 
+class Die
+
 	def initialize(sides)
 		@sides = sides
 	end
@@ -21,8 +22,13 @@ class Die
 	end
 end
 
-puts "We're rolling a six sided die!"
-puts Die.new(6).roll
+SIX_SIDED_DIE = Die.new(6)
+EIGHT_SIDED_DIE = Die.new(8)
+TEN_SIDED_DIE = Die.new(10)
+TWENTY_SIDED_DIE= Die.new(20)
 
-puts "Now we're rolling two 20 sided die!"
-puts Die.new(20).roll(6)
+puts "We're rolling a six sided die!"
+puts EIGHT_SIDED_DIE.roll
+
+# puts "Now we're rolling two 20 sided die!"
+# puts Die.new(20).roll(6)
