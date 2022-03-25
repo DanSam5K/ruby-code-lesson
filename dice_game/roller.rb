11973281756af176aecd2e52c515c5a5e3ba5c06
Daniel Samuel <dansam5000@gmail.com>
2022-03-25 13:21:58 +0100

    Roll any number of dice

--- dice_game/roller.rb
+++ dice_game/roller.rb
@@ -1,5 +1,18 @@
-def roll(sides)
-	rand(sides) + 1
+def roll(sides, number=1)
+	roll_array = []
+	number.times do
+    roll_value = rand(sides) + 1
+		roll_array << roll_value
+	end
+	total = 0
+	roll_array.each do |roll|
+		total += roll
+	end
+	total
 end
 
-puts roll(20)
+puts "We're rolling a six sided die!"
+puts roll(6)
+
+puts "Now we're rolling two 20 sided die!"
+puts roll(6, 2)
