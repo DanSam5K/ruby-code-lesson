c6c53c52c4b5b9f408eb7402c8b8ecca93da04cc
Daniel Samuel <dansam5000@gmail.com>
2022-03-26 12:12:50 +0100

    Dice game implementation

--- dice_game/roller.rb
+++ dice_game/roller.rb
@@ -22,13 +22,22 @@ class Die
 	end
 end
 
-SIX_SIDED_DIE = Die.new(6)
-EIGHT_SIDED_DIE = Die.new(8)
-TEN_SIDED_DIE = Die.new(10)
-TWENTY_SIDED_DIE= Die.new(20)
+# SIX_SIDED_DIE = Die.new(6)
+# EIGHT_SIDED_DIE = Die.new(8)
+# TEN_SIDED_DIE = Die.new(10)
+# TWENTY_SIDED_DIE= Die.new(20)
 
 puts "We're rolling a six sided die!"
 puts EIGHT_SIDED_DIE.roll
 
-# puts "Now we're rolling two 20 sided die!"
-# puts Die.new(20).roll(6)
+puts "Now we're rolling two 20 sided die!"
+puts Die.new(20).roll(6)
+
+puts "Let's play dice, shall we?"
+puts "How many sides do you want your die to have?"
+	choice = gets.chomp.to_i
+	puts "How many times do you want to roll the die?"
+	number = gets.chomp.to_i
+	puts "Your die has #{choice} sides, and you rolled it #{number} times."
+	puts Die.new(choice).roll(number)
+
