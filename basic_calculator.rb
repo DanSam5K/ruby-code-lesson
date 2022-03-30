6a24e5eb14cebf2445557fb8b89d52d1ed06f2f7
Daniel Samuel <dansam5000@gmail.com>
2022-03-30 08:16:38 +0100

    remove redundant files after refactoring

--- basic_calculator.rb
+++
@@ -1,7 +0,0 @@
-#buiding a basic calcultator
-puts "Enter a number: "
-num1 = gets.chomp.to_i #convert user input to integer
-puts "Enter another number: "
-num2 = gets.chomp.to_f #convert to floating number like fraction
-
-puts(num1 + num2)
