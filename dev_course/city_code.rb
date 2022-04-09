005ab15070e98f7df1d058eb417090f621f22cc4
Daniel Samuel <dansam5000@gmail.com>
2022-04-09 19:24:40 +0100

    Create a hash of cities and code

---
+++ dev_course/city_code.rb
@@ -0,0 +1,27 @@
+# 1. Create a dictionary (hash) of 10 cities and their corresponding city codes.
+
+
+# 2. Prompt the user to enter a city name. (hint: use gets.chomp)
+
+# 3. Print the city code of the city that the user entered. (hint: use the hash you created in step 1)
+
+# 4.  display area code based on city name
+
+# 5. Loop - keep the program running until the user enters the word "exit"
+
+# 6. Method to display the city code of a city that the user enters.
+
+# create a hash
+
+dial_book = {
+	"newyork" => "212",
+	"newbrunswick" => "732",
+	"edison" => "908",
+	"plainsboro" => "609",
+	"sanfrancisco" => "301",
+	"miami" => "305",
+	"paloalto" => "650",
+	"evanston" => "847",
+	"orlando" => "407",
+	"lancaster" => "717"
+}
